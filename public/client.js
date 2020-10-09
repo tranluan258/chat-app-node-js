9f6f2c58e8e02d98ada7042f5cdad3770fd58702
Tran Luan <tranluanqqq@gmail.com>
2020-10-09 20:41:30 +0700

    clean code

--- public/client.js
+++ public/client.js
@@ -1,73 +1,69 @@
-//call server
-const socket = io("http://localhost:8080");
+const socket = io("http://localhost:8080")
 
-//client nghe data tu server gui ve
 socket.on("sever-send-regis-fail", () => {
-    $(".alert-danger").html("Name already exists!");
-    $(".alert-danger").fadeIn(1000);
+    $(".alert-danger").html("Name already exists!")
+    $(".alert-danger").fadeIn(1000)
     setTimeout(() => {
-        $(".alert-danger").fadeOut();
-    }, 2000);
-});
+        $(".alert-danger").fadeOut()
+    }, 2000)
+})
 
-socket.on("server-send-regis-success", (data) => {
-    $("#name-user").html(data);
-    $(".login-box").hide(500);
-    $(".chat-box").show(1000);
-});
+socket.on("server-send-regis-success", data => {
+    $("#name-user").html(data)
+    $(".login-box").hide(500)
+    $(".chat-box").show(1000)
+})
 
-socket.on("server-send-list-user", (data) => {
-    $(".user").html("");
+socket.on("server-send-list-user", data => {
+    $(".user").html("")
     data.forEach((i) => {
-        $(".user").append("<div class='user-online'>" + i + "</div>");
-    });
-});
+        $(".user").append("<div class='user-online'>" + i + "</div>")
+    })
+})
 
-//client receive message
+socket.on("server-send-message-you", data => {
+    $(".body-chat").append("<div class='body-chat-you'><span class='data-chat-you'>" + data.un + ":" + data.nd + "</span></div>")
+})
 
-socket.on("server-send-message-you", (data) => {
-    $(".body-chat").append("<div class='body-chat-you'><span class='data-chat-you'>" + data.un + ":" + data.nd + "</span></div>");
-});
-
-socket.on("server-send-message-friend", (data) => {
-    $(".body-chat").append("<div class='body-chat-friend'><span class='data-chat-friend'>" + data.un + ":" + data.nd + "</span></div>");
-});
+socket.on("server-send-message-friend", data => {
+    $(".body-chat").append("<div class='body-chat-friend'><span class='data-chat-friend'>" + data.un + ":" + data.nd + "</span></div>")
+})
 
 $(document).ready(() => {
 
-    $(".login-box").show();
-    $(".chat-box").hide();
-    $(".alert-danger").hide();
+    $(".login-box").show()
+    $(".chat-box").hide()
+    $(".alert-danger").hide()
 
     $("#btn-registation").click(() => {
         if ($("#user-name").val() == "") {
-            $(".alert-danger").html("Please enter your name!");
-            $(".alert-danger").fadeIn(1000);
+            $(".alert-danger").html("Please enter your name!")
+            $(".alert-danger").fadeIn(1000)
             setTimeout(() => {
-                $(".alert-danger").fadeOut();
-            }, 2000);
+                $(".alert-danger").fadeOut()
+            }, 2000)
         } else {
-            socket.emit("Client-send-username", $("#user-name").val()); //khách hàng emit name lên server  với name là Client-send-data
-            $("#user-name").val("");
+            socket.emit("Client-send-username", $("#user-name").val())
+            $("#user-name").val("")
         }
-    });
+    })
 
     $("#btn-send").click(() => {
         if ($("#message").val() === "") {
 
         }
         else {
-            socket.emit("client-send-message", $("#message").val());
-            $("#message").val("");
+            socket.emit("client-send-message", $("#message").val())
+            $("#message").val("")
         }
-    });
+    })
 
     $("#btn-logout").click(() => {
-        socket.emit("client-logout");
-        $(".login-box").show(1000);
-        $(".chat-box").hide(500);
-    });
+        socket.emit("client-logout")
+        $(".login-box").show(1000)
+        $(".chat-box").hide(500)
+    })
 
-});
+})
 
 
--- server.js
+++ server.js
@@ -1,62 +1,54 @@
-//create server
-const express = require("express");
+const express = require("express")
 const app = express();
-app.use(express.static("./public")); // file user truy cap
-app.set("view engine", "ejs"); // set view cho home page
-app.set("views", "./views");
+app.use(express.static("./public"))
+app.set("view engine", "ejs")
+app.set("views", "./views")
 
+const server = require("http").Server(app)
+const io = require("socket.io")(server)
+const PORT = process.env.PORT || 8080;
+server.listen(PORT)
 
-const server = require("http").Server(app);
-//cau hinh socket io
-const io = require("socket.io")(server);
-server.listen(8080);
+let arrUser = []
 
-let arrUser = [];
-// check user connection (use io.on)
-io.on("connection", (socket) => {
-    console.log("connected :" + socket.id);
-
-    //check disconnect
+io.on("connection", socket => {
     socket.on("disconnect", () => {
         console.log("disconnected :" + socket.id);
         if (socket.name != undefined) {
             arrUser.splice(
                 arrUser.indexOf(socket.name), 1
             )
-            socket.broadcast.emit("server-send-list-user", arrUser);
+            socket.broadcast.emit("server-send-list-user", arrUser)
         }
-    });
-    //server on data cua client
-    socket.on("Client-send-username", (data) => {
-        // receive and check user name
+    })
+
+    socket.on("Client-send-username", data => {
         if (arrUser.indexOf(data) >= 0) {
-            socket.emit("sever-send-regis-fail");
+            socket.emit("sever-send-regis-fail")
         }
         else {
-            socket.name = data;
-            arrUser.push(data);//add user in arrUser
-            socket.emit("server-send-regis-success", data);
-            io.sockets.emit("server-send-list-user", arrUser);
+            socket.name = data
+            arrUser.push(data)
+            socket.emit("server-send-regis-success", data)
+            io.sockets.emit("server-send-list-user", arrUser)
         }
-    });
+    })
 
-    //receive message
     socket.on("client-send-message", (data) => {
-        socket.emit("server-send-message-you", { un: socket.name, nd: data });
-        socket.broadcast.emit("server-send-message-friend", { un: socket.name, nd: data });
-    });
+        socket.emit("server-send-message-you", { un: socket.name, nd: data })
+        socket.broadcast.emit("server-send-message-friend", { un: socket.name, nd: data })
+    })
 
-    //logout
     socket.on("client-logout", () => {
         arrUser.splice(
             arrUser.indexOf(socket.name), 1
         )
-        socket.broadcast.emit("server-send-list-user", arrUser);
-    });
-});
+        socket.broadcast.emit("server-send-list-user", arrUser)
+    })
+})
 
 
 
 app.get("/", (req, res) => {
-    res.render("homepage");
-});
+    res.render("homepage")
+})
