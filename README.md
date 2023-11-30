8a8722866ba7fb6e270444eba5ac4c5667f08e39
LuanTran <tranluanqqq@gmail.com>
2023-11-30 20:37:07 +0700

    update version and create README file

---
+++ README.md
@@ -0,0 +1,1 @@
+#Pracice socket io
--- server.js
+++ server.js
@@ -1,48 +1,57 @@
-require('dotenv').config
-const express = require("express")
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import http from "http";
+import * as socketio from "socket.io";
+
 const app = express();
-app.use(express.static("./public"))
-app.set("view engine", "ejs")
-app.set("views", "./views")
 
-const server = require("http").Server(app)
-const io = require("socket.io")(server)
-const PORT =  8080;
-server.listen(PORT)
+app.use(express.static("./public"));
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
-let arrUser = []
+const server = http.createServer(app);
+const io = new socketio.Server(server);
 
-io.on("connection", socket => {
-    socket.on("disconnect", () => {
-        if (socket.name != undefined) {
-            arrUser.splice(arrUser.indexOf(socket.name), 1)
-            socket.broadcast.emit("server-send-list-user", arrUser)
-        }
-    })
-    socket.on("Client-send-username", data => {
-        if (arrUser.indexOf(data) >= 0) {
-            socket.emit("sever-send-regis-fail")
-        }
-        else {
-            socket.name = data
-            arrUser.push(data)
-            socket.emit("server-send-regis-success", data)
-            socket.broadcast.emit("server-send-new-client", data)
-            io.sockets.emit("server-send-list-user", arrUser)
-        }
-    })
+const PORT = 8080;
+server.listen(PORT);
 
-    socket.on("client-send-message", (data) => {
-        socket.emit("server-send-message-you", { un: socket.name, nd: data })
-        socket.broadcast.emit("server-send-message-friend", { un: socket.name, nd: data })
-    })
+let arrUser = [];
 
-    socket.on("client-logout", () => {
-        arrUser.splice(arrUser.indexOf(socket.name), 1)
-        socket.broadcast.emit("server-send-list-user", arrUser)
-    })
-})
+io.on("connection", (socket) => {
+  socket.on("disconnect", () => {
+    if (socket.name != undefined) {
+      arrUser.splice(arrUser.indexOf(socket.name), 1);
+      socket.broadcast.emit("server-send-list-user", arrUser);
+    }
+  });
+  socket.on("Client-send-username", (data) => {
+    if (arrUser.indexOf(data) >= 0) {
+      socket.emit("sever-send-regis-fail");
+    } else {
+      socket.name = data;
+      arrUser.push(data);
+      socket.emit("server-send-regis-success", data);
+      socket.broadcast.emit("server-send-new-client", data);
+      io.sockets.emit("server-send-list-user", arrUser);
+    }
+  });
 
-app.get("/", (req, res) => {
-    res.render("homepage")
-})
+  socket.on("client-send-message", (data) => {
+    socket.emit("server-send-message-you", { un: socket.name, nd: data });
+    socket.broadcast.emit("server-send-message-friend", {
+      un: socket.name,
+      nd: data,
+    });
+  });
+
+  socket.on("client-logout", () => {
+    arrUser.splice(arrUser.indexOf(socket.name), 1);
+    socket.broadcast.emit("server-send-list-user", arrUser);
+  });
+});
+
+app.get("/", (_req, res) => {
+  res.render("homepage");
+});
+
